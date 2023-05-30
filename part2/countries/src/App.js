6157a8867f26ff683293c8c9a4ba0f26a308021f
chenxu <cxu@MacBook-Pro.local>
2023-05-30 12:05:09 +0300

    Move weather loading into a Child component

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -7,9 +7,9 @@ const App = () => {
   const [filterName, setFilter] = useState('')
   const [countries, setCountries] = useState(null)
   const [selectedCountry, setSelectedCountry] = useState(null)
-  const [weather, setWeather] = useState(null)
+  // const [weather, setWeather] = useState(null)
 
-  const api_key = process.env.REACT_APP_API_KEY
+  // const api_key = process.env.REACT_APP_API_KEY
 
   useEffect(() => {
     if (countries === null){     
@@ -19,33 +19,33 @@ const App = () => {
         .then(response => {
           console.log('promise fulfilled')
           console.log(response.data)
-          console.log('api_key', api_key)
+          // console.log('api_key', api_key)
           setCountries(response.data)
         })}
     
-    if (selectedCountry) {
-      console.log('fetching weather...')
-      // console.log('selectedCountry', selectedCountry.name.common)
-      // console.log('coordinates', selectedCountry.capitalInfo.latlng)
-      const [lat, lng] = selectedCountry.capitalInfo.latlng
-      console.log('lat', lat)
-      console.log('lng', lng)
-      axios
-        .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&units=metric&appid=${api_key}`)
-        .then(response => {
-          console.log('promise fulfilled')
-          console.log(response.data)
-          setWeather(response.data)
-          // console.log('weather', weather.current.weather[0].icon)
-        })
-      // axios
-      //   .get(`https://api.weatherstack.com/current?access_key=${api_key}&query=${selectedCountry.capital}`)
-      //   .then(response => {
-      //     console.log('promise fulfilled')
-      //     console.log(response.data)
-      //     setWeather(response.data)
-      //   })
-    }
+    // if (selectedCountry) {
+    //   console.log('fetching weather...')
+    //   // console.log('selectedCountry', selectedCountry.name.common)
+    //   // console.log('coordinates', selectedCountry.capitalInfo.latlng)
+    //   const [lat, lng] = selectedCountry.capitalInfo.latlng
+    //   console.log('lat', lat)
+    //   console.log('lng', lng)
+    //   axios
+    //     .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&units=metric&appid=${api_key}`)
+    //     .then(response => {
+    //       console.log('promise fulfilled')
+    //       console.log(response.data)
+    //       setWeather(response.data)
+    //       // console.log('weather', weather.current.weather[0].icon)
+    //     })
+    //   // axios
+    //   //   .get(`https://api.weatherstack.com/current?access_key=${api_key}&query=${selectedCountry.capital}`)
+    //   //   .then(response => {
+    //   //     console.log('promise fulfilled')
+    //   //     console.log(response.data)
+    //   //     setWeather(response.data)
+    //   //   })
+    // }
   }, [selectedCountry])
 
   if (countries === null) {
@@ -73,14 +73,14 @@ const App = () => {
         ? <CountriesDetail filtered={filtered} handleSet={handleSet}/>
         : <CountryNameList filtered={filtered} handleSet={handleSet}/>
 
-  const weatherToShow = weather && selectedCountry
-    ? <div>
-        <h2>Weather in {selectedCountry.capital}</h2>
-        <div>temperature {(weather.current.temp).toFixed(2)} Celsius</div>
-        <img src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}.png`} alt={weather.current.weather[0].description} />
-        <div>wind {weather.current.wind_speed} m/s</div>
-      </div>
-    : null
+  // const weatherToShow = weather && selectedCountry
+  //   ? <div>
+  //       <h2>Weather in {selectedCountry.capital}</h2>
+  //       <div>temperature {(weather.current.temp).toFixed(2)} Celsius</div>
+  //       <img src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}.png`} alt={weather.current.weather[0].description} />
+  //       <div>wind {weather.current.wind_speed} m/s</div>
+  //     </div>
+  //   : null
 
   return (
     <div>
@@ -88,7 +88,7 @@ const App = () => {
         find countries <input value={filterName} onChange={handleFiltered} />
       </div>
       {countriesToShow}
-      {weatherToShow}
+      {/* {weatherToShow} */}
     </div>
   )
 }
--- part2/countries/src/components/ShowOneCountry.js
+++ part2/countries/src/components/ShowOneCountry.js
@@ -1,12 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ShowOneCountry = ({ country , handleSet}) => {
   // console.log(country.capitalInfo.latlng)
   // useEffect(() => {
   //   setSelectedCountry(country);
   // }, []);
+  // useEffect(() => {
+  //   handleSet(country)
+  // }, [])
+  const [weather, setWeather] = useState(null)
+  const api_key = process.env.REACT_APP_API_KEY
+  
   useEffect(() => {
-    handleSet(country)
+    if (country) {
+      console.log('fetching weather...')
+      // console.log('selectedCountry', selectedCountry.name.common)
+      // console.log('coordinates', selectedCountry.capitalInfo.latlng)
+      const [lat, lng] = country.capitalInfo.latlng
+      console.log('lat', lat)
+      console.log('lng', lng)
+      axios
+        .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lng}&units=metric&appid=${api_key}`)
+        .then(response => {
+          console.log('promise fulfilled')
+          console.log(response.data)
+          setWeather(response.data)
+          // console.log('weather', weather.current.weather[0].icon)
+        })
+    }
   }, [])
 
   return (
@@ -21,6 +43,16 @@ const ShowOneCountry = ({ country , handleSet}) => {
       {Object.values(country.languages).map((language, index) => <li key={index}>{language}</li>)}
     </ul>
     <img src={country.flags.png} alt="flag" height="100" style={{marginTop: '10px'}}/>
+    {weather
+   ? (<div>
+       <h2>Weather in {country.capital}</h2>
+       <div>temperature {(weather.current.temp).toFixed(2)} Celsius</div>
+       <img src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}.png`} alt={weather.current.weather[0].description} />
+       <div>wind {weather.current.wind_speed} m/s</div>
+     </div>)
+   : (
+    <div>Loading weather...</div>
+     )}
     </div>
   )
 }
