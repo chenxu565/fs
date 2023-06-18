b1cd3d34f4df24264ee415cfd027353b04ebb657
ChenXu <chen.xu@ubuntu.vdi>
2023-06-18 17:37:52 +0300

    3.13 and 3.14 done Start to integrate MongoDB into the backend

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan') // import morgan
+require('dotenv').config()
+
+const Person = require('./models/person')
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -27,26 +34,6 @@ app.use(morgan(function (tokens, req, res) {
 }))
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
 ]
 
 app.get('/', (request, response) => {
@@ -55,26 +42,51 @@ app.get('/', (request, response) => {
 
 app.get('/info', (request, response) => {
     const date = new Date()
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+    Person.find({}).then(persons => {
+        // console.log(persons)
+        response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
     }
 )
+})
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  // console.log(body)
+  if (!body.name || !body.number) { 
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const person = persons.find(person => person.id === id)
-    console.log(person)
+  Person.findById(request.params.id).then(person => {
     if (person) {
-        response.json(person)
+      response.json(person)
     } else {
-        response.status(404).end()
+      response.status(404).end()
     }
-}
-)
+  })
+    .catch(error => {
+        console.log(error)
+        response.status(400).send({ error: 'malformatted id' })
+    })
+})
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -84,33 +96,7 @@ app.delete('/api/persons/:id', (request, response) => {
   }
 )
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    // console.log(body)
-    if (!body.name || !body.number) { 
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
-    }
-    
-    const found = persons.find(person => person.name === body.name)
-    // console.log('found?')
-    // console.log(found)
-    if (found) {
-        return response.status(400).json({ 
-            error: 'name must be unique' 
-          })
-    }
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: Math.floor(Math.random() * 10000),
-    }
-    // console.log(person)
-    persons = persons.concat(person)
-    response.json(person)
-  }
-)
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
