da5f14c345dca734babb732c9d9fef0e833f91e8
ChenXu <chen.xu@ubuntu.vdi>
2023-06-21 18:15:19 +0300

    3.15, 3.16, 3.17, 3.18 done

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ require('dotenv').config()
 
 const Person = require('./models/person')
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -72,28 +80,44 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-    .catch(error => {
-        console.log(error)
-        response.status(400).send({ error: 'malformatted id' })
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error)) // pass the error to the error handler middleware
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end() // 204 No Content
+    })
+    .catch(error => next(error))
+  }
+)
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  // console.log(body)
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  // console.log(person)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
   }
 )
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
