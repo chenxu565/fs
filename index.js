9bf5ae6715594ed97b0c5981716dacb73db05b14
ChenXu <chen.xu@ubuntu.vdi>
2023-06-22 17:54:15 +0300

    3.20, 3.21 done

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ require('dotenv').config()
 const Person = require('./models/person')
 
 const errorHandler = (error, request, response, next) => {
+  console.error(error.name)
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
   else if (error.name === 'ValidationError') {
+    // console.log('error.message', error.message)
     return response.status(400).json({ error: error.message })
   }
   next(error)
