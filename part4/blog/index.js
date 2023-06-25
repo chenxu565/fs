ec4fbf8374031e307240d2f297698585e6fd28a1
ChenXu <chen.xu@ubuntu.vdi>
2023-06-25 04:50:06 +0300

    Isolate models/blog.js

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -6,15 +6,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 logger.info('connecting to', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
