d3fd031deb0d301fd88b7d41b566ef388b3e36d7
ChenXu <chen.xu@ubuntu.vdi>
2023-06-25 11:28:29 +0300

    4.5 done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  const maxLikesBlog = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog, blogs[0])
+
+  return {
+    title: maxLikesBlog.title,
+    author: maxLikesBlog.author,
+    likes: maxLikesBlog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
