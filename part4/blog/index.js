b2d428ee07c34982fcd9d422f813cf85219988e5
ChenXu <chen.xu@ubuntu.vdi>
2023-06-25 03:37:15 +0300

    Isolate middleware.js

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,9 +1,11 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -25,6 +27,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -43,6 +46,9 @@ app.post('/api/blogs', (request, response) => {
       response.status(201).json(result)
     })
 })
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
