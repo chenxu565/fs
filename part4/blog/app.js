d02faf28a48690dbe3e2b04a97d9f5ae84deae54
ChenXu <chen.xu@ubuntu.vdi>
2023-06-27 18:11:00 +0300

    Re-order `require` at the beginning of `app.js`

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,12 +1,13 @@
-const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 require('express-async-errors')
+
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.set('strictQuery', false)
 
