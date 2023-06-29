43ea352b7f8b4c5e6eb83c5e50e3745b7507a972
ChenXu <chen.xu@ubuntu.vdi>
2023-06-29 17:40:54 +0300

    4.17 done
    Expand blogs so that each blog contains information on the creator of the blog.
    
    Modify adding new blogs so that when a new blog is created, any user from the database is designated as its creator (for example the one found first). Implement this according to part 4 chapter populate. Which user is designated as the creator does not matter just yet. The functionality is finished in exercise 4.19.
    
    Modify listing all blogs so that the creator's user information is displayed with the blog and listing all users also displays the blogs created by each user.

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
+    .populate('user', { username: 1 , name: 1, id: 1 })
   response.json(blogs)
 })
 
@@ -18,14 +20,20 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end()
   }
 
+  const user = await User.findOne({})
+
   const blog = new Blog({
+    url: body.url,
     title: body.title,
     author: body.author,
-    url: body.url,
+    user: user._id,
     likes: body.likes || 0
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
