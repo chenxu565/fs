91609bc03c72697a7d58ef292406199b4ae64da9
ChenXu <chen.xu@ubuntu.vdi>
2023-06-30 09:56:27 +0300

    4.19 done
    Modify adding new blogs so that it is only possible if a valid token is sent with the HTTP POST request. The user identified by the token is designated as the creator of the blog.

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -2,6 +2,17 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  // console.log('authorization', authorization)
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
     .populate('user', { username: 1 , name: 1, id: 1 })
@@ -16,11 +27,22 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  if (!body.title || !body.url) {
-    return response.status(400).end()
+  const token = getTokenFrom(request)
+  if (!token) {
+    return response.status(401).json({ error: 'token missing' })
+  }
+  // console.log('token', token)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  // console.log('decodedToken', decodedToken)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
-  const user = await User.findOne({})
+  if (!body.title || !body.url) {
+    return response.status(400).json({ error: 'title or url missing' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     url: body.url,
