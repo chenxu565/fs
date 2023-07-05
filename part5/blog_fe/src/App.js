1cec7772e19602dfc0d8af7d5e1a8698b2d8ba8f
ChenXu <chen.xu@ubuntu.vdi>
2023-07-05 08:19:20 +0300

    5.1 done
    Implement login functionality to the frontend. The token returned with a successful login is saved to the application's state user.
    
    If a user is not logged in, only the login form is visible.
    
    If the user is logged-in, the name of the user and a list of blogs is shown.

--- part5/blog_fe/src/App.js
+++ part5/blog_fe/src/App.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +18,80 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    console.log('loggedUserJSON', loggedUserJSON)
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      console.log('user', user)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+        const user = await loginService.login({
+          username, password,
+        })
+        blogService.setToken(user.token)
+        window.localStorage.setItem(
+          'loggedBlogappUser', JSON.stringify(user)
+        )
+        setUser(user)
+        setUsername('')
+        setPassword('')
+      } catch (exception) {
+        setErrorMessage('Wrong credentials')
+        console.log('exception', exception)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      { !user && 
+        <div>
+          <h2> log in to applicaiton</h2>
+          {loginForm()}
+        </div>
+      }
+      { user &&
+        <div>
+          <h2>blogs</h2>
+          <p>{user.name} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      }
     </div>
   )
 }
