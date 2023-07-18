a0a90d43ab31188efaae366a7b54d9f47469260b
ChenXu <chen.xu@ubuntu.vdi>
2023-07-18 11:02:33 +0300

    6.5 done
    Make that the anecdotes are ordered by the number of votes.

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -22,10 +22,12 @@ const App = () => {
     event.target.anecdote.value = ''
   }
 
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
