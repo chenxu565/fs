faa81dde2d6741a2198df3ec1b780fb4a3ca6a1d
ChenXu <chen.xu@ubuntu.vdi>
2023-07-18 11:42:55 +0300

    6.7 done
    Separate the creation of new anecdotes into a component called AnecdoteForm. Move all logic for creating a new anecdote into this new component.

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { voteOf, createAnecdote } from './reducers/anecdoteReducer'
+import { voteOf } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,13 +9,6 @@ const App = () => {
   const vote = (id) => {
     console.log('vote', id)
     dispatch(voteOf(id))
-  }
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    console.log('addAnecdote', event.target.anecdote.value)
-    dispatch(createAnecdote(event.target.anecdote.value))
-    event.target.anecdote.value = ''
   }
 
   const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
@@ -33,11 +27,7 @@ const App = () => {
           </div>
         </div>
       )}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote"/></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   )
 }
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,25 @@
+import { createAnecdote } from "../reducers/anecdoteReducer"
+import { useDispatch } from "react-redux"
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value
+    dispatch(createAnecdote(anecdote))
+    event.target.anecdote.value = ""
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <input name="anecdote" />
+        <button type="submit">add</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
