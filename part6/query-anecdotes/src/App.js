09a5978d6235b33d78be31a69f646489d21aaad1
ChenXu <chen.xu@ubuntu.vdi>
2023-07-22 11:28:43 +0300

    6.20 done
    Implement retrieving anecdotes from the server using React Query.
    
    The application  works in such a way that if there are problems communicating with the server, only an error page will be displayed.

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,19 +1,32 @@
+import { useQuery } from 'react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAnecdotes } from './requests'
 
 const App = () => {
+  const result = useQuery('anecdotes', getAnecdotes,
+    {
+      retry: 1,
+    }
+  )
+
+  console.log(result)
+
+  if (result.isLoading) {
+    return <div>Loading...</div>
+  }
+  else if (result.status === 'error') {
+    if (result.error.name === 'AxiosError') {
+      return <div>anecdote service not available due to problems in network</div>
+    }
+    return <div>error: {result.error.message}</div>
+  }
+
+  const anecdotes = result.data
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
-
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
 
   return (
     <div>
