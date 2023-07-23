8aa1d8e503d8d47a1255124c05c432356a097759
ChenXu <chen.xu@ubuntu.vdi>
2023-07-23 10:40:26 +0300

    7.1 done
    Add React Router to the application so that by clicking links in the Menu component the view can be changed.
    
    At the root of the application, meaning the path /, show the list of anecdotes
    
    The Footer component should always be visible at the bottom.
    
    The creation of a new anecdote should happen e.g. in the path create

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,4 +1,13 @@
 import { useState } from 'react'
+import {
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useParams,
+  useNavigate,
+  useMatch
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +15,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={padding} to="/anecdotes">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
     </div>
   )
 }
@@ -126,9 +135,16 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
+      {/* <AnecdoteList anecdotes={anecdotes} />
       <About />
-      <CreateNew addNew={addNew} />
+      <CreateNew addNew={addNew} /> */}
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/create" element={<CreateNew addNew={addNew} />} />
+        <Route path="/anecdotes" element={<Navigate to="/"/>} />
+        {/* <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes} />} /> */}
+      </Routes>
       <Footer />
     </div>
   )
