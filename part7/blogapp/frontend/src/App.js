5a3924469df48fc0a74c900da24fff20d53f5761
ChenXu <chen.xu@ubuntu.vdi>
2023-08-09 12:22:15 +0300

    7.14 and 7.15 done
    Implement a view to the application that displays all of the basic information related to users.
    
    Implement a view for individual user that displays all of the blog posts added by that user.
    
    One can access the view by clicking the name of the user in the view that lists all users.

--- part7/blogapp/frontend/src/App.js
+++ part7/blogapp/frontend/src/App.js
@@ -6,6 +6,10 @@ import Notification from './components/Notification'
 import LoggedUser from './components/LoggedUser'
 import TogglableBlogForm from './components/TogglableNewBlog'
 import Blogs from './components/Blogs'
+import Users from './components/Users'
+import User from './components/User'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { useSetStorageUser, useStoreValue } from './StoreContext'
 
@@ -30,13 +34,26 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h2>blogs</h2>
-      <Notification />
-      <LoggedUser />
-      <TogglableBlogForm />
-      <Blogs />
-    </div>
+    <Router>
+      <div>
+        <h2>blogs</h2>
+        <Notification />
+        <LoggedUser />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <TogglableBlogForm />
+                <Blogs />
+              </div>
+            }
+          />
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/:id" element={<User />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
