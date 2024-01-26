8bce90c9ca1a48dcd3998ac7aac5bd759cc3ef52
Chen Xu <cxu@MacBook-Pro.local>
2024-01-26 19:45:57 +0200

    Refactor update and delete functionality in Blog component

--- part7/blogapp/frontend/src/components/Blog.js
+++ part7/blogapp/frontend/src/components/Blog.js
@@ -27,11 +27,13 @@ const Blog = () => {
 
   const updateBlogMutation = useMutation(blogService.updateBlog, {
     onSuccess: (updatedBlog) => {
-      const blogs = queryClient.getQueryData('blogs')
-      queryClient.setQueryData(
-        'blogs',
-        blogs.map((blog) => (blog.id !== updatedBlog.id ? blog : updatedBlog)),
-      )
+      const currentBlogs = queryClient.getQueryData('blogs')
+      if (currentBlogs) {
+        queryClient.setQueryData(
+          'blogs',
+          currentBlogs.map((blog) => (blog.id !== updatedBlog.id ? blog : updatedBlog)),
+        )
+      }
     },
     onError: (error) => {
       console.log(error)
@@ -40,12 +42,14 @@ const Blog = () => {
 
   const deleteBlogMutation = useMutation(blogService.removeBlog, {
     onSuccess: (deletedBlog) => {
-      const blogs = queryClient.getQueryData('blogs')
-      queryClient.setQueryData(
-        'blogs',
-        blogs.filter((blog) => blog.id !== deletedBlog.id),
-      )
-      notifyWith(`Blog '${deletedBlog.title}' removed`)
+      const currentBlogs = queryClient.getQueryData('blogs')
+      if (currentBlogs) {
+        queryClient.setQueryData(
+          'blogs',
+          currentBlogs.filter((blog) => blog.id !== deletedBlog.id),
+        )
+        notifyWith(`Blog '${deletedBlog.title}' removed`)
+      }
     },
     onError: (error) => {
       console.log(error)
